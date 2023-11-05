9255391402685a1856eefd3881c19ec8506619a9
Mohammad Bakhshi <77542557+mohammad-bakhshi@users.noreply.github.com>
2023-11-05 20:56:35 +0330

    add frontend

---
+++ backend/prisma/migrations/20231105162019_hotel/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Hotel" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+
+    CONSTRAINT "Hotel_pkey" PRIMARY KEY ("id")
+);
